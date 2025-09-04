d62f1c5351a32bcb7c7cac54cb8f99edd3739507
Alphan <alphanmete22@gmail.com>
2025-09-04 15:30:07 +0200

    Added Lat and Long in Sensor Data

--- app/src/main/java/com/example/smartspaces_w1/SensorData.java
+++ app/src/main/java/com/example/smartspaces_w1/SensorData.java
@@ -14,10 +14,14 @@ import java.io.OutputStreamWriter;
 public class SensorData {
     private long time;
     private float value;
+    private float latitude;
+    private float longitude;
 
-    public SensorData(long time, float value) {
+    public SensorData(long time, float value, float latitude, float longitude) {
         this.time = time;
         this.value = value;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public long getTime() {
@@ -36,11 +40,31 @@ public class SensorData {
         this.value = value;
     }
 
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
     public void writeData(Context context) {
         String val = Float.toString(this.value);
         String time = Long.toString(this.time);
+        String lat = Float.toString(this.latitude);
+        String longt = Float.toString(this.longitude);
         String finalWrite = "Time=" + time + "\n" +
                             "Accel=" + val + "\n" +
+                            "Latitude=" + lat + "\n" +
+                            "Longitude=" + longt + "\n" +
                             "-\n";
         try {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("data.txt", Context.MODE_APPEND));
