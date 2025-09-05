9de67d52a7324d8eb4d06b690480ad44b0aa8cee
Dylan Sterling <d.t.sterling@student.utwente.nl>
2025-09-05 11:36:49 +0200

    Updated the moving average filter

--- app/src/main/java/com/example/smartspaces_w1/SensorFilter.java
+++ app/src/main/java/com/example/smartspaces_w1/SensorFilter.java
@@ -5,26 +5,26 @@ import java.util.List;
 
 public class SensorFilter {
     private List<Float> dataBuffer = new ArrayList<>();
-    private final float bufferSize = 10;
+    private final int bufferSize = 10;
+    private float total= 0;
+    private int current_index= 0;
+    private float avgVal = 0;
 
-    public void addToBuffer(float x)
-    {
-        dataBuffer.add(x);
-        if (dataBuffer.size() >= bufferSize)
-        {
-            // When buffer exceeds the allowed size, trim it
-            dataBuffer.remove(0);
-        }
+    public float getAvgVal() {
+        return avgVal;
     }
 
-    public float findAverage()
-    {
-        float avg = 0;
-        for (Float num : dataBuffer)
-        {
-            avg += num;
+
+    public float movingAverage(float newVal) {
+        if (dataBuffer.size() < bufferSize) {
+            total += newVal;
+            dataBuffer.add(newVal);
+        } else {
+            total = total - dataBuffer.get(current_index) + newVal;
+            dataBuffer.set(current_index, newVal);
+            current_index = (current_index + 1) % bufferSize;
         }
-        avg = avg / bufferSize;
-        return avg;
+        avgVal = total / dataBuffer.size();
+        return avgVal;
     }
 }
